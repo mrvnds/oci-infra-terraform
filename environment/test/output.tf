400f79d17fa479de51ac1900121417bf0c79ae91
Mervin Fernan De Sotto <mervin.fernan.de.sotto@oracle.com>
2025-10-22 18:07:20 +0800

    update output

--- environment/test/output.tf
+++ environment/test/output.tf
@@ -4,6 +4,13 @@ output "bastion_public_ip" {
 output "bastion_private_ip" {
   value = module.private_bastion.bastion_private_ips
 }
+output "db_details" {
+  value = {
+    db_name      = module.oracle_base_db.oracledb_name
+    address      = module.oracle_base_db.db_ip_address
+  }
+  
+}
 output "oke_cluster_id" {
   value = module.oke_cluster.oke_cluster_id
 }
--- modules/database/cdb/outputs.tf
+++ modules/database/cdb/outputs.tf
@@ -4,3 +4,8 @@ output "oracledb_name" {
 output "cdb_id" {
   value = oci_database_db_system.oracle_base_db.db_home[0].database[0].id
 }
+
+output "db_ip_address" {
+  value = oci_database_db_system.oracle_base_db.private_ip
+  
+}
